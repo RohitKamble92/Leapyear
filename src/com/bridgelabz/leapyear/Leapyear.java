23e75ab4ccaff1ffffe318e52027a5c7469453e1
Rohit <rohitkamble10792@gmail.com>
2022-05-22 14:51:01 +0530

    Checked whether Year is leap or not

--- src/com/bridgelabz/leapyear/Leapyear.java
+++ src/com/bridgelabz/leapyear/Leapyear.java
@@ -1,9 +1,41 @@
 package com.bridgelabz.leapyear;
 
-
+import com.bridgelabz.leapyear;
 
 public class Leapyear {
     public static void main(String[] args) {
-        System.out.println("Welcome to LeapYear Program");
+        // INITIALIZATION
+        int year, n;
+        String str;
+        Utility u = new Utility();
+
+        // INPUT DATA
+        System.out.print("enter the year: ");
+        str = u.inputString();
+        year = Integer.parseInt(str);
+
+        // COMPUTE THE LENGTH
+        n = str.length();
+
+        // CHECK LENGTH CONDITION
+        if (n != 4) {
+            System.out.println("enter year as 4 digit number");
+            return;
+        }
+
+        // COMPUTATION
+        else {
+            if (year % 400 == 0 && year % 100 == 0)
+                System.out.println("year " + year + " is a leap year");
+            else if (year % 100 != 0 && year % 4 == 0)
+                System.out.println("year " + year + " is a leap year");
+            else
+                System.out.println("year " + year + " is not a leap year");
+        }
+
     }
+
 }
+
+
+
